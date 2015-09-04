e0929b1c16205cafcd6a1be741d16af828c21493
Adam J. Penn <madannep@gmail.com>
2015-09-03 21:18:08 -0700

    got it all working minus it outputting grep at the begining of the content search. I will have tyson look at it tomorrow

--- rfind/rfind.rb
+++ rfind/rfind.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/env ruby
 
 userinput = ARGV[0]
-
-# Finds all files with userinput and splits them into an
-# array of strings.
-filenames = `find -iname *#{userinput}*`.split
-puts filenames
-
-files = filenames.map { |dir| dir =~ /\.(|(rb)|(txt)|(erb)|(js)|(css)|(html)|(yml))$/ }
-puts files
+matchingfiles = `find -iname *#{userinput}*`.split
+matchingfiles.keep_if { |dir| dir =~ /\.(|(rb)|(txt)|(erb)|(js)|(css)|(html)|(yml))$/ }
+matchingfiles.collect! { |x| "  " + x }
+puts "Files with names that matches <#{userinput}>", matchingfiles
+puts "**************************************************"
+allfiles = (`find .`.split).keep_if { |dir| dir =~ /\.(|(rb)|(txt)|(erb)|(js)|(css)|(html)|(yml))$/ }
+puts "Files with content that matches <#{userinput}>"
+allfiles.each do |filename|
+  matchingcontents = `grep -n -i #{userinput} #{filename}`.split("\n")
+  matchingcontents.collect! { |x| "  " + x }
+  if matchingcontents.size() > 0
+    puts filename
+    puts matchingcontents
+    puts "--------------------------------------------------"
+  end
+end
