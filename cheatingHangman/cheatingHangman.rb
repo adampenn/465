e1be99a8ff8f2fdf8adcdbf15ea09bc340f5100e
Adam J. Penn <madannep@gmail.com>
2015-09-16 22:54:40 -0700

    finished the cheatingHnagman game

--- cheatingHangman/cheatingHangman.rb
+++ cheatingHangman/cheatingHangman.rb
@@ -2,23 +2,58 @@
 
 dictionary = File.read("words").split
 
-puts "Enter word length between 5-20: "
+printf("Enter word length between 5-20: ")
 wordLength = gets.chomp().to_i
 
 if wordLength > 20
   wordLength = 20
-  puts "You inputed a number greater then 20 so I made it 20."
 elsif wordLength < 5
   wordLength = 5
-  puts "You inputed a number less then 5 so I made it 5."
 end
 
 dictionary.keep_if { |x| x.length == wordLength }
 
-numOfTries = 0
-while numOfTries < 10
-  puts "Enter your guess: "
-  guess = gets.chomp().to_c
-  dictionary.keep_if { |x| x.include? "#{guess}" == false }
-  numOfTries = numOfTries + 1
+allGuesses = []
+numOfTries = 9
+fair = false
+word = Array.new(wordLength, "_").join(" ")
+secretWord = nil
+
+while numOfTries >= 0
+  printf("Enter your guess: ")
+  guess = gets.chomp()
+  while allGuesses.include?("#{guess}") || guess.size != 1 || guess =~ /[[:digit:]]/
+    puts "Invlaid input"
+    guess = gets.chomp()
+  end
+  allGuesses.push(guess)
+  puts "(You have #{numOfTries} tries remaining)"
+  if !fair
+    backupDictionary = Array.new(dictionary)
+    dictionary.keep_if { |x| x.include?("#{allGuesses.last}") == false }
+    if dictionary.size == 0
+      fair = true
+      secretWord = backupDictionary.first
+      correctGuess = allGuesses.delete("#{allGuesses.last}")
+      word[secretWord.index("#{correctGuess}") * 2] = correctGuess
+      numOfTries = numOfTries + 1
+    end
+  end
+  if fair
+    if secretWord.include?("#{allGuesses.last}")
+      correctGuess = allGuesses.delete("#{allGuesses.last}")
+      word[secretWord.index("#{correctGuess}") * 2] = correctGuess
+      numOfTries = numOfTries + 1
+    end
+  end
+  puts allGuesses.join(" ")
+  puts word
+  if !word.include?("_")
+    puts "You win!"
+    numOfTries = -2
+  end
+  numOfTries = numOfTries - 1
+end
+if numOfTries == -1
+  puts "You're out of tries, you lose."
 end
